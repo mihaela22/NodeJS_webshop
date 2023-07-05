33d5de00f6639f147aff471ce0fa7ab8b3931702
Mihaela <mihaela.staraj@gmail.com>
2023-07-05 14:16:06 +0200

    Sequelize - defining a model, syncing JS definitions to the db

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "bulbasaur", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "bulbasaur",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
