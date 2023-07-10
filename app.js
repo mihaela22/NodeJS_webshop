1ed6d462f6202ad7fa5b19af7aa61d543815880a
Mihaela <mihaela.staraj@gmail.com>
2023-07-10 08:59:54 +0200

    association methods

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -35,6 +37,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" }); // delete products when user is deleted
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 //  .sync({ force: true })  override tables
 sequelize
