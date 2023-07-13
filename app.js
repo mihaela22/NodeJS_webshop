6c24781b783e68434c787eb5525c462f8cdc3930
Mihaela <mihaela.staraj@gmail.com>
2023-07-13 08:59:49 +0200

    MongoDB database connection

--- app.js
+++ app.js
@@ -5,74 +5,32 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const sequelize = require("./util/database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+const mongoConnect = require("./util/database");
 
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views"); // views templates are by default set in views
 
 const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" }); // delete products when user is deleted
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  // .sync({ force: true }) // override tables
-  .sync()
-  .then((result) => {
-    // console.log(result);
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "Wookiee", email: "wookiee@mail.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    // console.log(user);
-    user
-      .getCart()
-      .then((cart) => {
-        let userCart = cart;
-        if (!cart) {
-          userCart = user.createCart();
-        }
-        return userCart;
-      })
-      .then((cart) => {
-        app.listen(3000);
-      })
-      .catch((err) => console.log(err));
-  });
+mongoConnect(() => {
+  app.listen(3000);
+});
